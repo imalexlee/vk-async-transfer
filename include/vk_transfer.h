63ab863d4d01224c1aa2f4b2b21ba85f3d16b64c
imalexlee <alexdlee1217@gmail.com>
2025-12-01 21:58:56 -0500

    added initial buffer transfer on static queue

--- include/vk_transfer.h
+++ include/vk_transfer.h
@@ -2,44 +2,48 @@
 
 #include "common.h"
 
-
 #define CMD_BUF_COUNT 5
-#define QUEUE_ENTRIES_COUNT 50
+#define QUEUE_ENTRIES_COUNT 100
 
 typedef enum transfer_type {
-    TRANSFER_TYPE_BUFFER_2_BUFFER,
+    TRANSFER_TYPE_BUFFER_TO_BUFFER,
 } transfer_type;
 
 typedef union transfer_location {
     VkBuffer buffer;
-    VkImage image;
+    VkImage  image;
 } transfer_location;
 
 typedef struct transfer_request {
     transfer_location src;
     transfer_location dst;
-    transfer_type type;
+    transfer_type     type;
 } transfer_request;
 
+typedef struct buffer_to_buffer_request {
+    VkBuffer src;
+    VkBuffer dst;
+} buffer_to_buffer_request;
+
 typedef struct transfer_request_queue {
-    i32 front;
-    i32 back;
+    i32              front;
+    i32              back;
     transfer_request queue[QUEUE_ENTRIES_COUNT];
 
-    pthread_cond_t worker_notify_cond;
+    pthread_cond_t  worker_notify_cond;
     pthread_mutex_t mutex;
 } transfer_request_queue;
 
 typedef struct transfer_command_pool {
-    VkCommandPool pool;
+    VkCommandPool   pool;
     VkCommandBuffer buffers[CMD_BUF_COUNT];
-    VkFence fences[CMD_BUF_COUNT];
+    VkFence         fences[CMD_BUF_COUNT];
 } transfer_command_pool;
 
 typedef struct transfer_engine {
-    VkDevice vk_device;
-    VkQueue vk_queue;
-    transfer_command_pool command_pool;
+    VkDevice               vk_device;
+    VkQueue                vk_queue;
+    transfer_command_pool  command_pool;
     transfer_request_queue request_queue;
 
     pthread_t worker_thread;
--- src/vk_transfer.c
+++ src/vk_transfer.c
@@ -1,16 +1,16 @@
 
-#include <vulkan/vulkan.h>
 #include "vk_transfer.h"
-#include <stdatomic.h>
 
+#include <assert.h>
+#include <stdatomic.h>
+#include <vulkan/vulkan.h>
 
 static void enqueue_request(transfer_engine* engine, const transfer_request* request) {
     transfer_request_queue* queue = &engine->request_queue;
     pthread_mutex_lock(&queue->mutex);
 
-    u32 idx           = queue->back;
-    queue->back       = (queue->back + 1) % QUEUE_ENTRIES_COUNT;
-    queue->queue[idx] = *request;
+    queue->queue[queue->back] = *request;
+    queue->back               = (queue->back + 1) % QUEUE_ENTRIES_COUNT;
 
     pthread_cond_signal(&queue->worker_notify_cond);
     pthread_mutex_unlock(&queue->mutex);
@@ -26,10 +26,8 @@ static transfer_request dequeue_request(transfer_engine* engine) {
         pthread_cond_wait(&queue->worker_notify_cond, &queue->mutex);
     }
 
-    u32 idx      = queue->front;
-    queue->front = (queue->front + 1) % QUEUE_ENTRIES_COUNT;
-
-    transfer_request curr_request = queue->queue[idx];
+    transfer_request curr_request = queue->queue[queue->front];
+    queue->front                  = (queue->front + 1) % QUEUE_ENTRIES_COUNT;
 
     pthread_mutex_unlock(&queue->mutex);
 
@@ -37,7 +35,6 @@ static transfer_request dequeue_request(transfer_engine* engine) {
 }
 
 static i32 get_available_command_buffer_idx(const transfer_engine* engine) {
-
     i32 i = 0;
     while (1) {
         VkResult vk_res = vkGetFenceStatus(engine->vk_device, engine->command_pool.fences[i]);
@@ -52,16 +49,24 @@ static i32 get_available_command_buffer_idx(const transfer_engine* engine) {
     return i;
 }
 
+static void transfer_buffer_to_buffer(VkCommandBuffer cmd, const transfer_request* transfer_request) {
+    VkBufferCopy buffer_copy;
+    buffer_copy.srcOffset = 0;
+    buffer_copy.dstOffset = 0;
+    buffer_copy.size      = VK_WHOLE_SIZE;
+
+    vkCmdCopyBuffer(cmd, transfer_request->src.buffer, transfer_request->dst.buffer, 1, &buffer_copy);
+}
+
 void* worker(void* arg) {
     transfer_engine* engine = arg;
 
     while (!atomic_load(&engine->should_close)) {
         transfer_request req = dequeue_request(engine);
 
-        i32 cmd_idx = get_available_command_buffer_idx(engine);
-
-        VkFence fence       = engine->command_pool.fences[cmd_idx];
-        VkCommandBuffer cmd = engine->command_pool.buffers[cmd_idx];
+        i32             cmd_idx = get_available_command_buffer_idx(engine);
+        VkFence         fence   = engine->command_pool.fences[cmd_idx];
+        VkCommandBuffer cmd     = engine->command_pool.buffers[cmd_idx];
 
         vkResetFences(engine->vk_device, 1, &fence);
 
@@ -71,6 +76,14 @@ void* worker(void* arg) {
         vkBeginCommandBuffer(cmd, &cmd_buf_bi);
 
         // TODO: perform transfer depending on request type
+
+        switch (req.type) {
+        case TRANSFER_TYPE_BUFFER_TO_BUFFER:
+            transfer_buffer_to_buffer(cmd, &req);
+
+        default:
+            assert(0 && "unhandled transfer type");
+        }
 
         vkEndCommandBuffer(cmd);
 
@@ -83,7 +96,6 @@ void* worker(void* arg) {
 
     return NULL;
 }
-
 
 void transfer_engine_init(transfer_engine* engine, VkDevice device, u32 transfer_queue_family) {
     VkCommandPoolCreateInfo pool_ci;
@@ -120,5 +132,25 @@ void transfer_engine_init(transfer_engine* engine, VkDevice device, u32 transfer
     pthread_create(&engine->worker_thread, NULL, worker, engine);
     pthread_cond_init(&engine->request_queue.worker_notify_cond, NULL);
     pthread_mutex_init(&engine->request_queue.mutex, NULL);
+}
 
+void transfer_engine_deinit(transfer_engine* engine) {
+    pthread_mutex_destroy(&engine->request_queue.mutex);
+    pthread_cond_destroy(&engine->request_queue.worker_notify_cond);
+    pthread_join(engine->worker_thread, NULL);
+
+    for (i32 i = 0; i < CMD_BUF_COUNT; ++i) {
+        vkDestroyFence(engine->vk_device, engine->command_pool.fences[i], NULL);
+    }
+
+    vkDestroyCommandPool(engine->vk_device, engine->command_pool.pool, NULL);
+}
+
+void transfer_engine_copy_buffer_to_buffer(transfer_engine* engine, const buffer_to_buffer_request* buffer_transfer) {
+    transfer_request transfer_request;
+    transfer_request.type       = TRANSFER_TYPE_BUFFER_TO_BUFFER;
+    transfer_request.src.buffer = buffer_transfer->src;
+    transfer_request.dst.buffer = buffer_transfer->dst;
+
+    enqueue_request(engine, &transfer_request);
 }
